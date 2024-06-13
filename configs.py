b5c3218688c0a67f62f6044e8b7fd264056933eb
Jitendra7653 <96977015+Jitendra7653@users.noreply.github.com>
2024-06-13 11:56:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "mxlinks.cloud")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "fdeae20ea5f2cf7cc2607306b2a6a7adbf5e2066")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "1731432915"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Jitendra7653:s7Qyd5ZLsKQGRJ0w@cluster0.6qwzgtp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
