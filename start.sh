218448ada47b812d07bb22b4c76fa1801c2b33ed
Jitendra7653 <96977015+Jitendra7653@users.noreply.github.com>
2024-11-16 22:09:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
+echo "Cloning Repo...."
+git clone https://github.com/Jitendra7653/File-Store-bot /File-Store-bot
+cd /File-Store-bot
+echo "Starting Bot.... Please Wait."
 gunicorn app:app & python bot.py
