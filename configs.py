3afbe83d3b922e6284f9c5837e1845882a1e1710
Jitendra7653 <96977015+Jitendra7653@users.noreply.github.com>
2024-11-16 21:45:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config(object):
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "filexshare_bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002179247918"))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "mxlinks.cloud")
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "link.ai1t.in")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "fdeae20ea5f2cf7cc2607306b2a6a7adbf5e2066")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "1731432915"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
@@ -16,7 +16,7 @@ class Config(object):
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
-  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", False))
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
